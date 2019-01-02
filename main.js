62f951bb9ba9e2e579cf701f0c067e3f57ea2738
Alyssa Nolan <canalyssa@outlook.com>
2019-01-02 06:20:19 +0530

    Structured output and Colors

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const request = require("request-promise");
 
 // Include Custom Modules
+const colors = require("./customs/colors");
 const getHTML = require("./customs/get-html");
 const getKeyword = require("./customs/get-keyword");
 const extractMagnets = require("./customs/extract-magnets");
@@ -15,8 +16,15 @@ async function main(){
 
     let html = await getHTML(keyword);
     let compiledStuff = extractMagnets(html);
-    console.log(compiledStuff);
+    for(i = 0; i < compiledStuff.length; i++){
+      console.log(colors.Bright + colors.FgYellow + compiledStuff[i][1]);
+      console.log(colors.FgWhite + compiledStuff[i][3]);
+      console.log(colors.FgCyan + colors.Dim + compiledStuff[i][4]);
+      console.log(colors.Reset);
+    }
   }
+
+  console.log(colors.Reset);
 }
 
 // Calling Main (Haha! just a habit)
